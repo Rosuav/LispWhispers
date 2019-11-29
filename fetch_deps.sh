84169ca155b4497061979a3eee365b4d76c20cec
Chris Angelico <rosuav@gmail.com>
2019-11-29 20:41:34 +1100

    Import ComfyJS and start using it

---
+++ fetch_deps.sh
@@ -0,0 +1,4 @@
+# Fetch/update dependencies
+# Ideally, these would be dynamically fetched rather than committed here
+
+wget https://raw.githubusercontent.com/instafluff/ComfyJS/master/dist/comfy.js
--- lispwhispers.js
+++ lispwhispers.js
@@ -1,1 +1,10 @@
+import "./comfy.js"; const ComfyJS = window.ComfyJS;
 console.log("Hello, world");
+console.log(ComfyJS);
+
+ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
+  if( flags.broadcaster && command == "test" ) {
+    console.log( "!test was typed in chat" );
+  }
+}
+ComfyJS.Init( "Rosuav" );
