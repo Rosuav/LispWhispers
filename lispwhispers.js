0b09959822afdd6633edf53701264da4d98acc18
Chris Angelico <rosuav@gmail.com>
2019-11-29 20:48:21 +1100

    JS: Stub in some whisper handling

--- lispwhispers.js
+++ lispwhispers.js
@@ -1,10 +1,10 @@
 import "./comfy.js"; const ComfyJS = window.ComfyJS;
-console.log("Hello, world");
-console.log(ComfyJS);
 
-ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
-  if( flags.broadcaster && command == "test" ) {
-    console.log( "!test was typed in chat" );
-  }
-}
-ComfyJS.Init( "Rosuav" );
+ComfyJS.onWhisper = (user, message, flags, self, extra) => {
+	console.log("Received whisper from", user);
+	console.log(message);
+};
+
+ComfyJS.Init("Rosuav");
+const here = window.location.toString(); //TODO: Do I need to clean anything off before using it as a redirect?
+const url = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=q6batx0epp608isickayubi39itsckt&redirect_uri=${here}&scope=chat:read+whispers:read+whispers:edit`;
